65e7252ebc786936668d8349157e9c128dd29fb2
ahmed elbagory <ahmed.elbagory1@gmail.com>
2023-09-17 18:37:35 -0400

    module 1 day 5 homework

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,31 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Temperature: ");
+		String userInput = scanner.nextLine();
+		int startingTemperature = Integer.parseInt(userInput);
+
+		System.out.println("Is the temperature in Celsius or Fahrenheit?");
+		String degreeType = scanner.nextLine();
+		if(degreeType.equals("C")){
+			double temperatureFarenheit = startingTemperature * 1.8 + 32;
+			System.out.println("Successfully converted " + startingTemperature + " C to: " + temperatureFarenheit + " F.");
+
+		} else {
+			double temperatureCelsius = (startingTemperature - 32) / 1.8;
+			System.out.println("Successfully converted " + startingTemperature + " F to: " + temperatureCelsius + " C.");
+		}
+
+
+
+
+
+
 
 
 	}
