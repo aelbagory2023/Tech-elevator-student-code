28fcd08ceeb74a3e16034b49d1012d008cbabcc4
ahmed elbagory <ahmed.elbagory1@gmail.com>
2023-09-18 13:29:36 -0400

    module 1 day 5 homework

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,25 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter measurement: ");
+		String userInput = scanner.nextLine();
+		int startingMeasurement = Integer.parseInt(userInput);
 
+		System.out.println("Is the measurement in meters or Feet?");
+		String measurementType = scanner.nextLine();
+		if(measurementType.equals("m") || measurementType.equals("M")){
+			double measurementInFeet = startingMeasurement * 3.2808399;
+			System.out.println("Successfully converted " + startingMeasurement + " m to: " + measurementInFeet+ " f.");
+
+		} else if(measurementType.equalsIgnoreCase("f")) {
+			double measurementInMeters = (startingMeasurement * 0.3048);
+			System.out.println("Successfully converted " + startingMeasurement + " F to: " + measurementInMeters + " m.");
+		}
 	}
 
 }
--- module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
@@ -14,6 +14,8 @@ public class Lecture {
 		 * is not the case for most classes */
 
 		/* create an new instance of String using a literal */
+		String raccoons = "Raccoons";
+		System.out.println("My new string is " + raccoons);
 
 		
 		System.out.println();
@@ -22,19 +24,64 @@ public class Lecture {
 		System.out.println("******************************");
 		System.out.println();
 
+		System.out.println("---CHARAT (Character At) ---");
+		String name = "Celine Dion";
+		char first = name.charAt(0); //get the first character, like arrays we start at 0.
+		char fourth = name.charAt(3);
+		char last = name.charAt(name.length() -1); //get the last character.
+		System.out.println(name);
+		System.out.println(first);
+		System.out.println(fourth);
+		System.out.println(last);
+
+		System.out.println("---startsWith / endsWith ---");
+		boolean isLastNameDion = name.endsWith("Dion");
+		System.out.println("Is Celine Dion's last name 'Dion'" + isLastNameDion);
+
+		System.out.println("----contains----");
+		String hello = "Hello There!";
+		boolean containstHello = hello.contains("Hello");
+		boolean containsHelloLowercase = hello.contains("hello");
+
+		System.out.println("Does the string " + hello + "contain 'Hello'?" + containstHello);
+        System.out.println("Does the String " + hello + "contain 'hello'" + containsHelloLowercase);
+
 
 		/* Other commonly used methods:
 		 *
 		 * endsWith
 		 * startsWith
-		 * indexOf
-		 * lastIndexOf
+		 * indexOf - finds the first instance of another String inside of a String. returns an integer
+		 * lastIndexOf - finds the last instance of another String inside of a String.
 		 * length
-		 * substring
+		 * substring - extracts part of a String
 		 * toLowerCase
 		 * toUpperCase
-		 * trim
+		 * trim - trims whitespace from a string(trims Spaces from start and end)
 		 */
+
+
+		System.out.println("-------substring-------");
+		String longerString = "I bought more acetone.";
+		//substring has two parameters.
+		//The first parameter is the starting index (where to start the substring in the original string)
+		// the second is optional, you do not have to include it.
+		// the second index where you want to end the substring is the original string.
+		// if you do not use the second parameter, the substring extends to the end of the original string.
+
+		int indexOfAcetone = longerString.indexOf("acetone");
+		String acetone = longerString.substring(indexOfAcetone);
+		System.out.println(acetone);
+		String justacetone = longerString.substring(indexOfAcetone, longerString.length() - 1);
+		justacetone = longerString.substring(indexOfAcetone,indexOfAcetone + 7);
+		System.out.println(justacetone);
+		longerString = longerString.replace("acetone", "fire extinguisher");
+        System.out.println(longerString);
+
+		char[] helloArray = new char[] {'H','e','l','l','o'};
+		String hello1 = new String(helloArray);
+		String hello2 = new String(helloArray);
+
 
 		System.out.println();
 		System.out.println("**********************");
