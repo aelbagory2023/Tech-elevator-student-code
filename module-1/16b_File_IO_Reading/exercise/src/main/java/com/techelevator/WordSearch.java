c09f66b0b79df8b5aaf597f354b2f371d35d3f7f
Ahmed Elbagory <ahmed.elbagory1@gmail.com>
2023-10-03 14:59:54 -0400

    module 1 day 15 homework

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,20 +1,52 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 
 public class WordSearch {
 
 	// Use this scanner for all user input. Don't create additional Scanners with System.in
 	private final Scanner userInput = new Scanner(System.in);
 
+
 	public static void main(String[] args) {
 		WordSearch wordSearch = new WordSearch();
 		wordSearch.run();
+
 	}
 
 	public void run() {
 		/* Your code goes here */
 
+		System.out.print("What is the fully qualified name of the file that should be searched?");
+		String filePath = userInput.nextLine();
+		File newFile = new File(filePath);
+		System.out.print("What is the search word you are looking for?");
+		String searchWord = userInput.nextLine().toLowerCase();
+
+		try(Scanner fileScanner = new Scanner(newFile)) {
+
+
+
+			int lineNumber = 0;
+			while (fileScanner.hasNextLine()) {
+				lineNumber++;
+				String line = fileScanner.nextLine();
+				if (line.contains(searchWord)) {
+					System.out.println( lineNumber + ") " + line);
+
+
+				}
+			}
+
+			userInput.close();
+
+		} catch (FileNotFoundException e) {
+				System.out.println("File not Found: " + filePath);
+
+			}
 
 	}
 
--- module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
@@ -20,6 +20,45 @@ public class Lecture {
 		 * A new instance of File can be created from a String that contains a file system path
 		 */
 
+		System.out.println("Enter the path for a directory: ");
+		String path = userInput.nextLine();
+		System.out.println("The path you put in is: " + path);
+
+		File myCoolDirectory = new File(path); //identify a file or path that exists
+
+		if(myCoolDirectory.exists()) {
+			System.out.println("Name: " + myCoolDirectory.getName());
+			System.out.println("Path: " + myCoolDirectory.getAbsolutePath());
+			System.out.println("Size: "+ myCoolDirectory.length());
+			if(myCoolDirectory.isFile()) {
+				System.out.println("This is a file!");
+			} else if (myCoolDirectory.isDirectory()) {
+				System.out.println("This is a Directory!");
+			}
+		}
+		//lets write to a file
+
+		//we first make a file to write to
+		System.out.println("Lets put a file in the directory that you put in!");
+		System.out.println("What do you want the name of your file to be?");
+		String filename = userInput.nextLine();
+
+		File newFile = new File(myCoolDirectory, filename);  //actaully make a file by passing in the path (location), and the file name
+		System.out.println("Name: " + newFile.getName());
+		System.out.println("Path: " + newFile.getAbsolutePath());
+		System.out.println("Size: " + newFile.length());
+		//all I have at this point is an object in memory, not a new file on my file system.
+		 newFile.createNewFile();
+
+		 // now for the writing part
+		System.out.println("what do you want to write in your file?");
+		String message = userInput.nextLine();
+		//open a new printWriter  to the new file
+
+		try (PrintWriter pw = new PrintWriter(newFile)) {
+			pw.println(message);
+		}
+
 
 
 	}
