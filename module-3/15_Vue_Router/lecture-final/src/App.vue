26d3c1f4d864ec6b15814d18421615697dbbba7d
Ben Langhinrichs <benjamin@techelevator.com>
2023-11-27 12:26:19 -0500

    Lecture-final and exercise for M3/Day 15

---
+++ module-3/15_Vue_Router/lecture-final/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <h1>Hello</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
---
+++ module-3/15_Vue_Router/lecture-final/src/views/AddReviewView.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Add Review</h1>
+    <add-review v-bind:productId="productIdentifier" greeting="Wow, you are adding a review!" />
+</template>
+
+<script>
+import AddReview from '../components/AddReview.vue';
+
+export default {
+    components: {
+        AddReview
+    },
+    computed: {
+        productIdentifier() {
+            return this.$route.params.id
+        }
+    }
+};
+
+</script>
